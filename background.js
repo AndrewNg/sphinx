cdeb3ca81f1447f1f65a17d1a46dab5b5b1971a1
Sabar Dasgupta <sabar@orchive.com>
2014-02-15 22:19:35 -0500

    background.js almost working

--- background.js
+++ background.js
@@ -6,14 +6,18 @@ chrome.runtime.onMessage.addListener(
 	function(request, sender, sendResponse) {
 		console.log("message received");
 		if (request.greeting == "snapshot") {
-			navigator.getUserMedia({video: true}, function(stream) {
-				var video = document.getElementById("v");
-				var canvas = document.getElementById("c");
+			navigator.webkitGetUserMedia({video: true}, function(stream) {
+				document.body.innerHTML += '<video id="v" width="300" height="300"></video>';
+				document.body.innerHTML += '<canvas id="c" style="display:none;" width="300" height="300"></canvas>';
+
+				var video = document.getElementById('v');
+				var canvas = document.getElementById('c');
 
 				video.src = stream;
-				canvas.getContext("2d").drawImage(video, 0, 0, 380, 300, 0, 0, 380, 300);
+				canvas.getContext("2d").drawImage(video, 0, 0, 300, 300, 0, 0, 300, 300);
 				var img = canvas.toDataURL("image/png");
-				sendResponse({farewell: img});
+				console.log(img);
+				sendResponse({farewell: "test"});
 			}, function(err) { alert("there was an error " + err)});
 		}
 });
